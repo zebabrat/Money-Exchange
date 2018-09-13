571d08f5a6a473c7eb21ccdb54a9116503c5fa3f
Андрей Забавко <zeba.brat@Zebabrat.local>
2018-09-14 01:38:21 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
     // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let objExchange = {};
+    let count = currency;
+
+    if (count <= 0) {
+        return {}
+    }
+
+    if (count > 0 && count <= 10000) {
+        if (count >= 50) {
+            objExchange.H = Math.trunc(count / 50);
+            count = count % 50;
+        }
+        if (count >= 25) {
+            objExchange.Q = Math.trunc(count / 25);
+            count = count % 25;
+        }
+        if (count >= 10) {
+            objExchange.D = Math.trunc(count / 10);
+            count = count % 10;
+        }
+        if (count >= 5) {
+            objExchange.N = Math.trunc(count / 5);
+            count = count % 5;
+        }
+        if (count >= 1) {
+            objExchange.P = Math.trunc(count / 1);
+        }
+    }
+
+    if (count > 10000) {
+        return { error: "You are rich, my friend! We don't have so much coins for exchange" }
+    }
+
+    return objExchange;
 }
